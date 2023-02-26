6f0aa8a12366a9e472448e4bba189f5527a18508
zanoo <dmitryellison@gmail.com>
2023-02-27 00:16:28 +0300

    Created get_substring()

--- solution/src/utils/utils.c
+++ solution/src/utils/utils.c
@@ -2,6 +2,12 @@
 
 #include <string.h>
 
+char* get_substring(const char* const str, size_t position) {
+    char* result[MAX_STRING_SIZE];
+    while (str[position] != '\0') {
+
+    }
+}
 
 uint8_t check_file_format(const char* const path) {
     char *ext = strrchr(path, '.');
