694700f657506050ba87184206e332dfe1544350
zanoo <dmitryellison@gmail.com>
2023-02-27 03:35:37 +0300

    Creates structure

--- solution/src/utils/utils.c
+++ solution/src/utils/utils.c
@@ -2,14 +2,8 @@
 
 #include <string.h>
 
-char* get_substring(const char* const str, size_t position) {
-    return str + position;
-}
-
-uint8_t check_file_format(const char* const path) {
+int check_file_format(const char* const path) {
     char *ext = strrchr(path, '.');
 
-    if (ext && !strcmp(++ext, "bmp")) return 1;
-
-    return 0;
+    return (ext == NULL) ? 0 : 1;
 }
